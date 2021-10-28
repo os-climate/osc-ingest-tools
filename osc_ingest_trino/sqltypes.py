372908907679507d4261cc2bbdc5f33eb49282a0
Erik Erlandson <eerlands@redhat.com>
2021-10-28 12:49:22 -0700

    sqltypes.py

---
+++ osc_ingest_trino/sqltypes.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+_p2smap = {
+    'string': 'varchar',
+    'Float64': 'double',
+    'Int64': 'bigint'
+}
+
+def pandas_type_to_sql(pt):
+    st = _p2smap.get(pt)
+    if st is not None:
+        return st
+    raise ValueError("unexpected pandas column type '{pt}'".format(pt=pt))
+
+# add ability to specify optional dict for specific fields?
+# if column name is present, use specified value?
+def create_table_schema_pairs(df):
+    if not isinstance(df, pd.DataFrame):
+        raise ValueError("df must be a pandas DataFrame")
+    ptypes = [str(e) for e in df.dtypes.to_list()]
+    stypes = [pandas_type_to_sql(e) for e in ptypes]
+    pz = list(zip(df.columns.to_list(), stypes))
+    return ",\n".join(["    {n} {t}".format(n=e[0],t=e[1]) for e in pz])
+
+
