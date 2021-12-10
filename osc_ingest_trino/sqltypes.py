bd06cc88b728363c63ab7ac102c17d5b4e950fd0
Erik Erlandson <eerlands@redhat.com>
2021-12-10 16:55:33 -0700

    expose typemap, add colmap

--- osc_ingest_trino/sqltypes.py
+++ osc_ingest_trino/sqltypes.py
@@ -17,7 +17,7 @@ _p2smap = {
     'Int64': 'bigint',
     'bool': 'boolean',
     'category': 'varchar',
-    'datetime64[ns, UTC]': 'timestamp(6)',
+    'datetime64[ns, UTC]': 'timestamp',
 }
 
 def pandas_type_to_sql(pt, typemap={}):
@@ -29,11 +29,11 @@ def pandas_type_to_sql(pt, typemap={}):
         return st
     raise ValueError("unexpected pandas column type '{pt}'".format(pt=pt))
 
-def create_table_schema_pairs(df, typemap = {}, indent = 4):
+def create_table_schema_pairs(df, typemap = {}, colmap = {}, indent = 4):
     if not isinstance(df, pd.DataFrame):
         raise ValueError("df must be a pandas DataFrame")
-    ptypes = [str(e) for e in df.dtypes.to_list()]
-    stypes = [pandas_type_to_sql(e, typemap=typemap) for e in ptypes]
-    pz = list(zip(df.columns.to_list(), stypes))
-    return ",\n".join([f"{' '*indent}{e[0]} {e[1]}" for e in pz])
-
+    if not isinstance(colmap, dict):
+        raise ValueError("colmap must be a dict")
+    columns = df.columns.to_list()
+    types = [colmap.get(col, pandas_type_to_sql(str(df[col].dtype), typemap=typemap)) for col in columns]
+    return ",\n".join([f"{' '*indent}{e[0]} {e[1]}" for e in zip(columns, types)])
