5c2c71e7b2f4e3946139760e6708aa68ab1bca34
Erik Erlandson <eerlands@redhat.com>
2022-02-24 13:32:05 -0700

    release 0.3.0

--- osc_ingest_trino/__init__.py
+++ osc_ingest_trino/__init__.py
@@ -5,7 +5,7 @@ python tools to assist with standardized data ingestion workflows for the OS-Cli
 """
 
 # defines the release version for this python package
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 
 from .sqlcols import *
 from .sqltypes import *
