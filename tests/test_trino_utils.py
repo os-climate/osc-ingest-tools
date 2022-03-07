72bfed70e2fb68c5237f0caeaaf3397fb140cabf
Nathan Gillett <negillett@gmail.com>
2022-03-07 16:36:16 -0500

    Setup tox testing (#16)
    Added tox.ini, an initial unit test, and supporting configuration.
    
    Developers may now execute tox jobs;
    - `tox -e py3`: runs pytest in a Python 3 environment
    - `tox -e static`: runs static checks (black, isort, flake8, mypy)
    - `tox -e cov`: runs pytest and produces an HTML coverage report
    - `tox`: runs all environment listed in 'envlist' (py38, static)
    
    Signed-off-by: Nathan Gillett <ngillett@redhat.com>

---
+++ tests/test_trino_utils.py
@@ -0,0 +1,20 @@
+import mock
+from osc_ingest_trino import attach_trino_engine
+
+
+@mock.patch("osc_ingest_trino.trino_utils.trino.auth.JWTAuthentication")
+@mock.patch("osc_ingest_trino.trino_utils.create_engine")
+def test_attach_trino_engine(mock_engine, mock_trino_auth, monkeypatch):
+    monkeypatch.setenv("TEST_USER", "tester")
+    monkeypatch.setenv("TEST_PASSWD", "supersecret123")
+    monkeypatch.setenv("TEST_HOST", "example")
+    monkeypatch.setenv("TEST_PORT", "8000")
+
+    fake_engine = mock.MagicMock()
+    fake_engine.connect.return_value = None
+    mock_engine.return_value = fake_engine
+    mock_trino_auth.return_value = "yep"
+
+    attach_trino_engine(env_var_prefix="TEST", catalog="ex_catalog", schema="ex_schema", verbose=True)
+
+    mock_engine.assert_called_with("trino://tester@example:8000/ex_catalog/ex_schema", connect_args={'auth': 'yep', 'http_scheme': 'https'})
