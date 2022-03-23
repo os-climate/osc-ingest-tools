e459f11d934cdd5c2ba587f8cef53f7267813744
Erik Erlandson <eerlands@redhat.com>
2022-03-23 15:17:25 -0700

    unit test TrinoBatchInsert
    Signed-off-by: Erik Erlandson <eerlands@redhat.com>

--- tests/test_trino_utils.py
+++ tests/test_trino_utils.py
@@ -1,6 +1,6 @@
-import mock
+from unittest import mock
 
-from osc_ingest_trino import attach_trino_engine
+from osc_ingest_trino import TrinoBatchInsert, attach_trino_engine
 
 
 @mock.patch("osc_ingest_trino.trino_utils.trino.auth.JWTAuthentication")
@@ -21,3 +21,22 @@ def test_attach_trino_engine(mock_engine, mock_trino_auth, monkeypatch):
     mock_engine.assert_called_with(
         "trino://tester@example:8000/ex_catalog/ex_schema", connect_args={"auth": "yep", "http_scheme": "https"}
     )
+
+
+def test_trino_batch_insert():
+    # mock up an sqlalchemy table
+    tbl = mock.MagicMock()
+    tbl.name = "test"
+    # mock up an sqlalchemy Connnection
+    cxn = mock.MagicMock()
+    # tuple data, in form supplied to __call__ as specified in 'method' param docs:
+    # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_sql.html
+    rows = [("a", 4.5), ("b", 1.1), ("c'd", 3.5)]
+    # invoke the __call__ method, simulating df.to_sql call
+    tbi = TrinoBatchInsert(catalog="test", schema="test", batch_size=2, verbose=True)
+    tbi(tbl, cxn, [], rows)
+
+    assert cxn.execute.call_count == 2
+    xcalls = cxn.execute.call_args_list
+    assert xcalls[0].args[0].text == "insert into test.test.test values\n('a', 4.5),\n('b', 1.1)"
+    assert xcalls[1].args[0].text == "insert into test.test.test values\n('c''d', 3.5)"
