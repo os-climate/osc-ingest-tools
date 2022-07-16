fdcddd6af061e93a6d11ba94f476dc2d423b4f2e
Erik Erlandson <eerlands@redhat.com>
2022-07-16 15:46:35 -0700

    fast_pandas_ingest_via_hive
    Signed-off-by: Erik Erlandson <eerlands@redhat.com>

--- osc_ingest_trino/__init__.py
+++ osc_ingest_trino/__init__.py
@@ -2,7 +2,7 @@ from .boto3_utils import attach_s3_bucket, upload_directory_to_s3
 from .dotenv_utils import load_credentials_dotenv
 from .sqlcols import enforce_partition_column_order, enforce_sql_column_names, sql_compliant_name
 from .sqltypes import create_table_schema_pairs, pandas_type_to_sql
-from .trino_utils import TrinoBatchInsert, attach_trino_engine
+from .trino_utils import TrinoBatchInsert, attach_trino_engine, fast_pandas_ingest_via_hive
 
 __all__ = [
     "sql_compliant_name",
@@ -14,5 +14,6 @@ __all__ = [
     "upload_directory_to_s3",
     "load_credentials_dotenv",
     "attach_trino_engine",
+    "fast_pandas_ingest_via_hive",
     "TrinoBatchInsert",
 ]
